21e9b69be87674781eaca849f1878c2c6943c6c1
Jaime <jaimeloeuf@gmail.com>
2020-05-21 04:20:56 +0800

    Rewrote the API to use ES6 class: 1.  Functionality still more or less the same but updated the API, to     support data isolation and multi instance use of this module. 2.  Added support for deep merging to add custom init objects without     worrying about overriding default values caused by shallow merges     using Object.assign

--- src/index.js
+++ src/index.js
@@ -3,15 +3,10 @@
  * @author JJ
  */
 
+import deepmerge from "deepmerge";
+
 // Throws an error and prevent module from being used if fetch is not available
 if (!window.fetch) throw new Error("FETCH API NOT AVAILABLE ON BROWSER");
-
-// Default internal error handler function that can be overwritten via setup()
-let _errorHandler = (error) => {
-  console.error(error);
-  // Allow error to bubble back to function caller
-  return error;
-};
 
 /**
  * Parse the body of the response (can either be JSON or text) to an object with type indicating properties
@@ -32,35 +27,10 @@ async function getParsedResponse(response) {
 }
 
 /**
- * Inner fetch function used to prepend API base URL and parse the response
- * @function _fetch
- * @param {String} _apiUrl path of the API only, the base API will be prepended
- * @param {String} url path of the API only, the base API will be prepended
- * @param {object} init Request object required by fetch
- */
-async function _fetch(_apiUrl, url = "", init) {
-  try {
-    // Call window fetch with prepended API URL and default request object
-    const response = await window.fetch(_apiUrl + url, init);
-
-    const parsedResponse = await getParsedResponse(response);
-
-    /* Return base on type of body data and include status code along side */
-    if (parsedResponse.json)
-      return { ...parsedResponse.response, statusCode: response.status };
-    else if (parsedResponse.string)
-      return { body: parsedResponse.response, statusCode: response.status };
-    else throw new Error("Invalid body type. Neither JSON nor String");
-  } catch (error) {
-    return _errorHandler(error);
-  }
-}
-
-/**
  * Only returns authentication header if user is authenticated.
  * Split out so if user is unauthenticated, this does not throw if currenUser is null
  * @function getAuthHeader
- * @param {function} firebaseAuth Firebase auth method
+ * @param {function} [firebaseAuth] Firebase auth method
  * @returns {String} Authentication header or nothing.
  */
 async function getAuthHeader(firebaseAuth) {
@@ -68,93 +38,130 @@ async function getAuthHeader(firebaseAuth) {
     return `Bearer ${await firebaseAuth().currentUser.getIdToken()}`;
 }
 
-/**
- * GET curried function that takes a init object before an URL
- */
-function _get(apiUrl, init = {}, firebaseAuth) {
-  return async function (url) {
-    return _fetch(
-      apiUrl,
-      url,
-      Object.assign(
-        {
-          method: "GET",
-          headers: {
-            Authorization: await getAuthHeader(firebaseAuth),
-          },
-        },
-        init
-      )
-    );
-  };
+function defaultErrorHandler(error) {
+  // Default internal error handler function that can be overwritten via setup()
+
+  // @todo Add a environment check to only do this if not production
+  console.error(error);
+  // Allow error to bubble back to function caller
+  return error;
 }
 
 /**
- * POST curried function that takes a init object before an URL and data
+ * Suggested to import the package under the "api" name to avoid collision with window.fetch
+ *
+ * @example
+ * api.get(url)
+ * api.modify(custom request object).get(url)
+ * api.post(url, data)
+ *
+ * @notice GET/POST methods are basically _get/_post with no init objects applied
  */
-function _post(apiUrl, init = {}, firebaseAuth) {
-  return async function (url, data) {
-    if (data) init.body = JSON.stringify(data);
+export default class fetch {
+  /**
+   * @param {function} firebaseAuth The same firebase auth function that ran initializeApp()
+   * @param {string} apiUrl Base API URL
+   * @param {function} [errorHandler] Error handling function for when the fetch failed
+   */
+  constructor(firebaseAuth, apiUrl, errorHandler) {
+    if (!firebaseAuth)
+      throw new Error("Firebase Auth is required for this to work");
 
-    return _fetch(
-      apiUrl,
-      url,
-      Object.assign(
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: await getAuthHeader(firebaseAuth),
-          },
-        },
-        init
-      )
-    );
-  };
-}
+    // Internal firebase auth instance that must be set to share the same auth instance between this library and your app
+    this._firebaseAuth = firebaseAuth;
 
-/**
- * Module's methods only exported through setup.
- * @function setup
- * @param {function} firebaseAuth The same firebase auth function that ran initializeApp()
- * @param {string} apiUrl Base API URL
- * @param {function} errorHandler Error handling function for when the fetch failed
- */
-export default function setup(firebaseAuth, apiUrl, errorHandler) {
-  // Internal firebase auth instance that must be set to share the same auth instance between this library and your app
-  // Set the module's internal base api URL
+    // Set the module's internal base api URL
+    this._apiUrl = apiUrl;
 
-  // Only set the error handler if defined, else keep the default handler
-  if (errorHandler) _errorHandler = errorHandler;
+    // Only set the error handler if defined, else keep the default handler
+    if (errorHandler) this._errorHandler = errorHandler;
+    else this._errorHandler = defaultErrorHandler;
+
+    this.get = this._get(undefined, firebaseAuth);
+    this.post = this._post(undefined, firebaseAuth);
+  }
 
   /**
-   * Function to modify the init object only once before making a new request
-   * @param {object} init Request object for fetch
-   * @returns {object} Same API object with custom request object partially applied.
+   * Inner fetch function used to prepend API base URL and parse the response
+   * @function _fetch
+   * @param {String} url path of the API only, the base API will be prepended
+   * @param {object} init Request object required by fetch
    */
-  function modify(init = {}, _firebaseAuth = firebaseAuth) {
-    // Return the http methods to chain it and make a request
-    return {
-      get: _get(apiUrl, init, _firebaseAuth),
-      post: _post(apiUrl, init, _firebaseAuth),
+  async _fetch(url = "", init) {
+    try {
+      // Call window fetch with prepended API URL and default request object
+      const response = await window.fetch(this._apiUrl + url, init);
+
+      const parsedResponse = await getParsedResponse(response);
+
+      /* Return base on type of body data and include status code along side */
+      if (parsedResponse.json)
+        return { ...parsedResponse.response, statusCode: response.status };
+      else if (parsedResponse.string)
+        return { body: parsedResponse.response, statusCode: response.status };
+      else throw new Error("Invalid body type. Neither JSON nor String");
+    } catch (error) {
+      return this._errorHandler(error);
+    }
+  }
+
+  /**
+   * GET curried function that takes a init object before an URL
+   */
+  _get(init = {}) {
+    // Arrow function to inherit "this", without using explicit "this" binding
+    return async (url) =>
+      this._fetch(
+        url,
+        deepmerge(
+          {
+            method: "GET",
+            headers: {
+              Authorization: await getAuthHeader(this._firebaseAuth),
+            },
+          },
+          init
+        )
+      );
+  }
+
+  /**
+   * POST curried function that takes a init object before an URL and data
+   */
+  _post(init = {}) {
+    // Arrow function to inherit "this", without using explicit "this" binding
+    return async (url, data) => {
+      if (data) init.body = JSON.stringify(data);
+
+      return this._fetch(
+        url,
+        deepmerge(
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: await getAuthHeader(this._firebaseAuth),
+            },
+          },
+          init
+        )
+      );
     };
   }
 
   /**
-   * Fetch methods
-   * @notice Suggested to import the package under the "api" name to avoid collision with window.fetch
+   * Function to modify init object only once before making a new request
+   * @param {object} init Request object for fetch
+   * @returns {object} Same API object with custom request object partially applied.
    *
    * @example
-   * api.get(url)
-   * api.modify(custom request object).get(url)
-   * api.post(url, data)
    * api.modify(custom request object).post(url, data)
-   *
-   * @notice GET/POST methods are basically _get/_post with no init objects applied
    */
-  return {
-    get: _get(apiUrl, undefined, firebaseAuth),
-    post: _post(apiUrl, undefined, firebaseAuth),
-    modify,
-  };
+  modify(init) {
+    // Return the http methods to chain it and make a request
+    return {
+      get: this._get(init),
+      post: this._post(init),
+    };
+  }
 }
